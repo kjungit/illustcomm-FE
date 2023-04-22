f4172f8e6442ee8fb34af7139de2eb02e16e2772
kjun <kjun_all@naver.com>
2023-04-23 05:13:20 +0900

    :sparkles: Create PicLabPage UI, masonry Layout

--- src/pages/PicLabPage/index.tsx
+++ src/pages/PicLabPage/index.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import PostItem from "../../common/PostItem";
+import Masonry from "react-masonry-css";
+import { PicLabWrapper } from "./style";
+
+const breakpointBlogPostColumnsObj = {
+  default: 2,
+  1200: 1,
+};
 
 function PicLabPage() {
-  return <div>PicLabPage</div>;
+  return (
+    <PicLabWrapper>
+      <Masonry
+        breakpointCols={breakpointBlogPostColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid-column"
+      >
+        <PostItem src={"../public/mainbanner_1.jpeg"} />
+        <PostItem src={"../public/basic.jpeg"} />
+        <PostItem src={"../public/basic.jpeg"} />
+        <PostItem src={"../public/mainbanner_2.jpeg"} />
+        <PostItem src={"../public/basic.jpeg"} />
+        <PostItem src={"../public/mainbanner_1.jpeg"} />
+      </Masonry>
+    </PicLabWrapper>
+  );
 }
 
 export default PicLabPage;
---
+++ src/pages/PicLabPage/style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const PicLabWrapper = styled.div`
+  .my-masonry-grid {
+    display: flex;
+    width: auto;
+  }
+  .my-masonry-grid-column {
+    padding: 0 60px;
+    background-clip: padding-box;
+  }
+  .my-masonry-grid-column > li {
+    margin-bottom: 130px;
+  }
+`;
