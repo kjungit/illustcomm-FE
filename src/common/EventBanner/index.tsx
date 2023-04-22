60ab9708d53328c26f9d202fcd50ad57b5211b03
kjun <kjun_all@naver.com>
2023-04-22 19:37:42 +0900

    :sparkles: Create EventComponent

---
+++ src/common/EventBanner/index.tsx
@@ -0,0 +1,11 @@
+import { BannerWrapper } from "./style";
+
+function EventBanner() {
+  return (
+    <BannerWrapper>
+      <img src="../public/main-event-banner.png" alt="" />
+    </BannerWrapper>
+  );
+}
+
+export default EventBanner;
---
+++ src/common/EventBanner/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+export const BannerWrapper = styled.div`
+  max-width: 1200px;
+  img {
+    width: 100%;
+    transform: scale(1);
+  }
+  padding: 60px 0 0;
+`;
