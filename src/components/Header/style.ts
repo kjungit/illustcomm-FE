561d79273cb368a4ea1df242c599f799efc6bca9
kjun <kjun_all@naver.com>
2023-04-28 07:15:58 +0900

    :sparkles: Create Header Component
    Logo, Profile, Login, Logout Data Connection

---
+++ src/components/Header/style.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.div`
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+export const TopWrapeer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 30px 20px 10px;
+  max-width: 1200px;
+  margin: auto;
+`;
+export const Logo = styled.div`
+  img {
+    width: 200px;
+  }
+  margin-right: auto;
+`;
+
+export const MyProfile = styled.div`
+  justify-self: flex-end;
+  cursor: pointer;
+`;
