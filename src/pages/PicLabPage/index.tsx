526a139af96c9c5f11c7cf724a79c14817f19a09
kjun <kjun_all@naver.com>
2023-04-28 07:51:23 +0900

    :recycle: Edit PicLabPage

--- src/pages/PicLabPage/index.tsx
+++ src/pages/PicLabPage/index.tsx
@@ -1,6 +1,8 @@
 import PostItem from "../../common/PostItem";
 import Masonry from "react-masonry-css";
 import { PicLabWrapper } from "./style";
+import { getPosts } from "../../apis/services/Post";
+import { useQuery } from "react-query";
 
 const breakpointBlogPostColumnsObj = {
   default: 2,
@@ -8,6 +10,15 @@ const breakpointBlogPostColumnsObj = {
 };
 
 function PicLabPage() {
+  const { data: posts, error, isLoading } = useQuery("posts", getPosts);
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+
+  if (error || !posts) {
+    return <div>error...</div>;
+  }
+
   return (
     <PicLabWrapper>
       <Masonry
@@ -15,12 +26,22 @@ function PicLabPage() {
         className="my-masonry-grid"
         columnClassName="my-masonry-grid-column"
       >
-        <PostItem src={"../public/mainbanner_1.jpeg"} />
-        <PostItem src={"../public/basic.jpeg"} />
-        <PostItem src={"../public/basic.jpeg"} />
-        <PostItem src={"../public/mainbanner_2.jpeg"} />
-        <PostItem src={"../public/basic.jpeg"} />
-        <PostItem src={"../public/mainbanner_1.jpeg"} />
+        {posts.map((post) => {
+          return (
+            <PostItem
+              key={post.id}
+              post={{
+                id: post.id,
+                body: post.body,
+                src: post.image,
+                alt: post.title,
+                likes: post.likes,
+                comments: post.comments.length,
+                author: post.author,
+              }}
+            />
+          );
+        })}
       </Masonry>
     </PicLabWrapper>
   );
