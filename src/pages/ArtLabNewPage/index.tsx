15fd93e8dfcb1980d87c002204109fa1ec0ca82c
kjun <kjun_all@naver.com>
2023-07-20 19:05:49 +0900

    Remove: 사용하지 않은 폴더 삭제

--- src/pages/ArtLabNewPage/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ArtLabNewPage() {
-  return <div>ArtLabNewPage</div>;
-}
-
-export default ArtLabNewPage;
--- src/pages/IllLabNewPage/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function IllLabNewPage() {
-  return <div>IllLabNewPage</div>;
-}
-
-export default IllLabNewPage;
